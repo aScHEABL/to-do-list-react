0c6adc57bf693fd40ec44c3e228d3dbcdf394991
aScHEABL <22256864+aScHEABL@users.noreply.github.com>
2023-06-18 05:06:45 +0800

    init: ContextAPI logics

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,27 +1,53 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, ReactNode } from "react";
 
-const AppContext = createContext(null);
+interface State {
+    test: any[];
+}
+
+interface ContextValue {
+    state: State;
+    dispatch: React.Dispatch<any>;
+}
+
+interface Action {
+    type: string;
+    payload?: any;
+}
 
 const initialState = {
+  test: [],
+};
 
+const AppContext = createContext<ContextValue>({} as ContextValue);
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "TEST":
+      return {
+        ...state,
+        test: action.payload.test,
+      };
+    default:
+      return state;
+  }
+};
+
+interface AppContextProviderProps {
+    children: ReactNode;
 }
 
-const reducer = (state, action) => {
+const AppContextProvider = ({ children }: AppContextProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const contextValue = {
+    state,
+    dispatch,
+  };
 
-}
+  return (
+    <AppContext.Provider value={contextValue}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
-const AppContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const contextValue = {
-        state,
-        dispatch,
-    }
-
-    return (
-        <AppContext.Provider value={contextValue}>
-            {...children}
-        </AppContext.Provider>
-    )
-}
-
-export { AppContext, AppContextProvider }
+export { AppContext, AppContextProvider };
